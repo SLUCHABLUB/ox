20738c7c0cff9ad85574ddb5582f5f0ad1335217
SLUCHABLUB <nelli.skogman@gmail.com>
2025-07-09 22:09:39 +0200

    Add the super modifier.

--- src/config/keys.rs
+++ src/config/keys.rs
@@ -79,6 +79,9 @@ pub fn key_to_string(modifiers: KMod, key: KCode) -> String {
     if modifiers.contains(KMod::SHIFT) {
         result += "shift_";
     }
+    if modifiers.contains(KMod::SUPER) {
+        result += "super_";
+    }
     result += &match key {
         KCode::Char('\\') => "\\\\".to_string(),
         KCode::Char('"') => "\\\"".to_string(),
